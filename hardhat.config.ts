8f9639a5ff4c70f7208dfd672e3ee1b14a2e4792
solidoracle <solidoracle17@gmail.com>
2022-10-18 12:58:25 +0200

    config file style

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,18 +1,16 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
 
 dotenv.config();
 
 // Defining this manually since ethers cannot be access from within the hardhat config
-const hashZero =
-  "0x0000000000000000000000000000000000000000000000000000000000000000";
+const hashZero = '0x0000000000000000000000000000000000000000000000000000000000000000';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.13",
+  solidity: '0.8.13',
   networks: {
     hardhat: {
       accounts: {
